ca5d584d7c99cb437b6d3d11bf195e0c6344b223
Chris <le.liquideur01@gmail.com>
2021-08-23 21:57:01 +0200

    P2C4 terminé

---
+++ src/compenents/PlantItem.js
@@ -0,0 +1,18 @@
+import CareScale from './CareScale'
+import '../styles/PlantItem.css'
+import { plantList } from '../datas/plantList'
+
+function PlantItem({ id, cover, name, water, light }) {
+	return (
+		<li key={id} className='lmj-plant-item'>
+			<img className='lmj-plant-item-cover' src={cover ? cover: 'https://images.prismic.io/figaroimmo/78de4361-486f-48e1-9914-4910c6f8127d_plantes-vertes.jpg?auto=compress,format&rect=0,0,1000,667&w=720&h=480'} alt={`${name} cover`} /> 
+			{name}
+			<div>
+				<CareScale careType='water' scaleValue={water} />
+				<CareScale careType='light' scaleValue={light} />
+			</div>
+		</li>
+	)
+}
+
+export default PlantItem
--- src/datas/plantList.js
+++ src/datas/plantList.js
@@ -1,51 +1,73 @@
 export const plantList = [
 	{
 		name: 'monstera',
+		cover: 'https://github.com/OpenClassrooms-Student-Center/7008001-Debutez-avec-React/blob/P2C4-Solution/src/assets/monstera.jpg?raw=true',
 		category: 'classique',
-		id: '1ed'
+		id: '1ed',
+		isBestSale: true,
+		light: 2,
+		water: 3
 	},
 	{
 		name: 'ficus lyrata',
+		cover: 'https://cdn.shopify.com/s/files/1/1063/7402/products/3_62e80caa-75be-4c9a-bd14-55f3f52e8133_500x.jpg?v=1613132374',
 		category: 'classique',
-		id: '2ab'
+		id: '2ab',
+		light: 3,
+		water: 1
 	},
 	{
 		name: 'pothos argenté',
+		cover: 'https://cdn.shopify.com/s/files/1/0004/2654/1108/products/LIVRAISONPLANTEPOTHOSSILVEREPIPREMNUMAUREUMSCINDAPSUS_1800x1800.jpg?v=1617830429',
 		category: 'classique',
 		id: '3sd',
-		isSpecialOffer: true
+		light: 1,
+		water: 2
 	},
 	{
 		name: 'yucca',
+		cover: 'https://images.truffaut.com/ARTICLES/JPG/0063000/63101_002_1000.jpg',
 		category: 'classique',
-		id: '4kk'
+		id: '4kk',
+		light: 3,
+		water: 1
 	},
 	{
 		name: 'olivier',
 		category: 'extérieur',
 		id: '5pl',
-		isAvailable: false
+		light: 3,
+		water: 1
 	},
 	{
 		name: 'géranium',
 		category: 'extérieur',
-		id: '6uo'
+		id: '6uo',
+		light: 2,
+		water: 2
 	},
 	{
 		name: 'basilique',
 		category: 'extérieur',
 		id: '7ie',
-		isSpecialOffer: true
+		isSpecialOffer: true,
+		isBestSale: true,
+		light: 2,
+		water: 3
 	},
 	{
 		name: 'aloe',
+		cover: 'https://www.jardiland.com/media/catalog/product/cache/image/470x470/e9c3970ab036de70892d86c6d221abfe/8/7/8715495035636_1.jpg',
 		category: 'plante grasse',
 		id: '8fp',
-		isSpecialOffer: true
+		light: 2,
+		water: 1
 	},
 	{
 		name: 'succulente',
 		category: 'plante grasse',
-		id: '9vn'
+		id: '9vn',
+		light: 2,
+		water: 1
 	}
 ]
